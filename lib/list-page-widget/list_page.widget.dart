53b76131a74dade5c8775a2445878280f0cdd9e6
mkeiji <keiji@kxps.pc>
2022-01-01 04:35:08 -0600

    IS-0 add item dialog

--- lib/list-page-widget/list_page.widget.dart
+++ lib/list-page-widget/list_page.widget.dart
@@ -14,14 +14,81 @@ class ListPageWidget extends StatefulWidget {
 /* STATE
 ---------------------------------------------------------------------*/
 class _ListPageWidgetState extends State<ListPageWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _textEditingController = TextEditingController();
+
   // Todo: this should come from api
   List<Item> items = [];
   int _counter = 0;
 
-  void _addItem() {
+  Future<void> showAddItemDialog(BuildContext context) async {
+    String placeholder = 'Enter item name';
+    String? input;
+
+    Form dialogForm = Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: _textEditingController,
+              validator: (value) {
+                return value!.isNotEmpty ? null : placeholder;
+              },
+              onChanged: (change) => {input = change},
+              decoration: InputDecoration(
+                hintText: placeholder,
+              ),
+            ),
+          ],
+        ));
+
+    ElevatedButton cancelBtn = ElevatedButton(
+      onPressed: () => Navigator.of(context).pop(),
+      child: const Text('Cancel'),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.red,
+      ),
+    );
+
+    ElevatedButton okBtn = ElevatedButton(
+      onPressed: () => {
+        if (_formKey.currentState!.validate())
+          {
+            _addItem(input),
+            _textEditingController.clear(),
+            Navigator.of(context).pop(),
+          }
+      },
+      child: const Text('OK'),
+    );
+
+    return await showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: (context, setState) {
+            return AlertDialog(
+              content: dialogForm,
+              title: const Text('Add Item'),
+              actions: <Widget>[
+                cancelBtn,
+                okBtn,
+              ],
+            );
+          });
+        });
+  }
+
+  void _addItem(String? name) {
+    String itemName = 'item $_counter';
+
+    if (name != null) {
+      itemName = name;
+    }
+
     setState(() {
       // Todo: should call api
-      items.add(Item(_counter, 'item $_counter', 1));
+      items.add(Item(_counter, itemName, 1));
       _counter++;
     });
   }
@@ -58,7 +125,9 @@ class _ListPageWidgetState extends State<ListPageWidget> {
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _addItem,
+        onPressed: () async {
+          await showAddItemDialog(context);
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
